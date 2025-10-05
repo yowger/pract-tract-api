26dfaf46554559bd65cc10b73164b6088cfdfd06
Ver0018 <rogerpantilyowger@gmail.com>
2025-10-06 04:45:24 +0800

    migration fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -29,12 +30,59 @@ class AuthController extends Controller
             'is_active' => $fields['is_active'] ?? true,
         ]);
 
-        if ($user->isStudent()) {
+        if ($user->isDirector()) {
+            $user->director()->create([]);
+        } elseif ($user->isAdvisor()) {
+            $user->advisor()->create(attributes: []);
+        } elseif ($user->isAgent()) {
+            $user->agent()->create([]);
+        } elseif ($user->isStudent()) {
             $user->student()->create([
                 'student_id' => $request->input('student_id'),
                 'program' => $request->input('program'),
                 'section' => $request->input('section'),
             ]);
         }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'User registered successfully',
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string|email|exists:users,email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $fields['email'])->first();
+
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response()->json([
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'User logged in successfully',
+            'user' => $user,
+            'token' => $token,
+        ], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'User logged out successfully',
+        ], 200);
     }
 }
