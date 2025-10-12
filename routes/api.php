af806ba21f606d07d005105b6929e5f8531d9564
Ver0018 <rogerpantilyowger@gmail.com>
2025-10-13 01:28:42 +0800

    updated sancutim config and routes

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,8 @@ use App\Http\Controllers\SectionController;
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('web')->post('/register', [AuthController::class, 'register']);
+Route::middleware('web')->post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
