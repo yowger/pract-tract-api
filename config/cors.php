12aa620a0d226831a16042a7cb8c9cfd08922939
Ver0018 <rogerpantilyowger@gmail.com>
2025-12-01 01:26:20 +0800

    updated cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173',],
+    'allowed_origins' => ['http://localhost:5173', 'https://pract-tract-git-main-yowgers-projects.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
